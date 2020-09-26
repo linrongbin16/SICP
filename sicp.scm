4d2e2d8b03a781b02d23255c452a136fd89f6b0f
linrongbin <linrongbin16@outlook.com>
2020-09-26 19:28:29 +0800

    update

--- sicp.scm
+++ sicp.scm
@@ -292,11 +292,105 @@
   (fixed-point (lambda (y) (average y (/ x y)))
                1.0))
 
-(sicp)
-; (format #t "fixed-point cos 1.0 = ~a~%" (fixed-point cos 1.0))
-; (format #t "fixed-point y=sin(y)+cos(y) 1.0 = ~a~%" (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0))
-; (format #t "fixed-point based sqrt 4.0 = ~a~%" (sqrt 4.0))
-; (format #t "fixed-point based sqrt 11.0 = ~a~%" (sqrt 11.0))
+; (sicp)
+; (print "fixed-point cos 1.0 = ~a~%" (fixed-point cos 1.0))
+; (print "fixed-point y=sin(y)+cos(y) 1.0 = ~a~%" (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0))
+; (print "fixed-point based sqrt 4.0 = ~a~%" (sqrt 4.0))
+; (print "fixed-point based sqrt 11.0 = ~a~%" (sqrt 11.0))
 
 (define (average-damp f)
   (lambda (x) (average x (f x))))
+
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+; (print "average-damp square 10 = " ((average-damp square) 10))
+; (print "sqrt 10 = " (sqrt 10))
+; (print "cube-root 10 = " (cube-root 10))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (/ x y))
+                            average-damp
+                            1.0))
+
+; (print "sqrt 10 = " (sqrt 10))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+                            newton-transform
+                            1.0))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+; (print "sqrt 10 = " (sqrt 10))
+
+(define (linear-combination a b x y)
+  (+ (* a x) (* b y)))
+
+(define (linear-combination a b x y)
+  (add (mul a x) (mul b y)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define x (cons 1 2))
+(define y (cons 3 4))
+(define z (cons x y))
+
+; (print "car x = " (car x) ", cdr x = " (cdr x))
+; (print "car z = " (car z) ", cdr z = " (cdr z))
+; (print "(car (car z)) = " (car (car z)) ", (cdr (cdr z)) = " (cdr (cdr z)))
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+(define one-third (make-rat 1 3))
+(print-rat one-third)
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (mul-rat one-third one-third))
